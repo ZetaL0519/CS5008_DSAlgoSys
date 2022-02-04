decae368bb02c1c834f9aceff359668c267f3326
ZetaL0519 <zetaleelsy19@gmail.com>
2022-02-04 18:39:43 -0500

    final version

--- Lab3_CStrings/part1/mystring.c
+++ Lab3_CStrings/part1/mystring.c
@@ -3,21 +3,38 @@
 // Include the declarations of our string functions
 #include "mystring.h"
 
-// search: 'man strlen' to see what function should do
+// return the length of string input
 size_t my_strlen(const char* input){
-    // IMPLEMENT
-    return 0;   
+    size_t length = 0;
+    while(*input != '\0'){
+        length ++;
+        *input ++;
+    }
+    return length;   
 }
 
-// search: 'man strcmp' to see what function should do
+// compare two strings
+// if they are the same, return 0
+// it src1 is larger, return > 0; src1 smaller, return <0
 int my_strcmp(const char* src1, const char* src2){
-    // IMPLEMENT
-    return 0;   
+    while (*src1 != '\0' && *src2 != '\0'){
+        if (*src1 != *src2){
+            break;
+        }
+        *src1 ++;
+        *src2 ++;
+    }
+    return *(const unsigned char*)src1 - *(const unsigned char*)src2;   
 }
 
-// search: 'man strchr' to see what function should do
+// return the first character c that appears in src1
 char* my_strchr(const char* src1, int c){
-    // IMPLEMENT
+    while (*src1 != c && *src1 != '\0'){
+        *src1 ++;
+    }
+    if (*src1 == c){
+        return (char*)src1;
+    }
     return NULL;   
 }
 
@@ -27,6 +44,14 @@ char* my_strchr(const char* src1, int c){
 //       I would like you to return a 'heap allocated' string
 //       that appends both of src1 and src2 into a new buffer
 char* my_string_append(const char* src1, const char* src2){
-    // IMPLEMENT
-    return NULL;   
+    char* res = (char*) malloc(my_strlen(src1) + my_strlen(src2));
+    int i = 0;
+    while (*src1 != '\0'){
+        res[i ++] = *src1++;
+    }
+    while (*src2 != '\0'){
+        res[i ++] = *src2++;
+    }
+    res[i] = '\0';
+    return res;   
 }
