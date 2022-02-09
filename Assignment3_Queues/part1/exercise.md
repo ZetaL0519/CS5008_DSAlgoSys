bc5c5a7cff45c9c6686ba5ed07a1b777b5ffbf15
ZetaL0519 <zetaleelsy19@gmail.com>
2022-02-09 16:11:48 -0500

    first version20220209

--- Assignment3_Queues/part1/exercise.md
+++ Assignment3_Queues/part1/exercise.md
@@ -1,4 +1,13 @@
 Why is a ring buffer useful and/or when should it be used?
 
-*edit your answer here*
+1. What is Circular Queue(ring buffer)?
+Circular Queue(ring buffer) is a linear data structure in which the operations are performes based on FIFO(First In First Out) principle and the last position is connected back to the first position to make a circle.
 
+2. Why is ring buffer useful?
+(1) Ring buffers are useful as fixed-sized queues. The fixed size is beneficial for embedded systems, as developers often try to use static data storage methods rather than dynamic allocations.
+(2) Ring buffers are useful for situations where data production and consumption happen at different rates: the most recent data is always available. 
+
+3. When should it be used?
+(1) The unused memory locations in the case of ordinary queues can be utilized in circular queues.
+(2) Traffic system: In computer controlled traffic system, circular queues are used to switch on the traffic lights one by one repeatedly.
+(3) CPU scheduling: Operating systems often maintain a queue of processes that are ready to execute or that waiting for a particular event to occur
--- Assignment3_Queues/part1/queue_t.c
+++ Assignment3_Queues/part1/queue_t.c
@@ -1,44 +1,97 @@
 #include "queue_t.h"
-#include <stdlib.h> // malloc/free
+#include <stdlib.h> 
 
 // Create a queue
 queue_t* create_queue(unsigned int _capacity){
-    return NULL; // TODO: change this code
+	// allocate space for queue and data array.
+    queue_t* queue = (queue_t*) malloc(sizeof(queue_t));
+	int* data_int = (int*) malloc(_capacity * sizeof(int));
+	// initialize front and back to -1.
+	queue ->front = -1;
+	queue ->back = -1;
+	queue ->capacity = _capacity;
+	queue ->size = 0;
+	queue ->data = data_int;
+	return queue;
 }
-
-
 
 // Queue Empty
 int queue_empty(queue_t* q){
-
+	if (q ->size == 0) {
+		return 1;
+	}
 	return 0;
 }
 
 // Queue Full
 int queue_full(queue_t* q){
-
+	if (q ->size == q ->capacity) {
+		return 1;
+	}
 	return 0;
 }
 
 // Enqueue a new item
 int queue_enqueue(queue_t* q, int item){
-		return -1; // Note: you should have two return statements in this function.
+	if (queue_full(q) == 1) {
+		return -1;
+	}
+	else {
+		unsigned int tail;
+		unsigned int capacity;
+		tail = q -> back;
+		capacity = q -> capacity;
+		int* data_int;
+		data_int = q ->data;
+		// store item in the according position in data array.
+		data_int[(tail + 1) % capacity] = item;
+		// update tail and back
+		tail = (tail + 1) % capacity;
+		q -> back = tail;
+		q ->size ++;
+		return 0;
+	}
 }
 
 // Dequeue an item
 int queue_dequeue(queue_t *q){
-
-		return 99999; // Note: This line is a filler so the code compiles.
+	if (queue_empty(q) == 1) {
+		exit(1);
+	}
+	else {
+		unsigned int head;
+		unsigned int capacity;
+		head = q -> front;
+		capacity = q -> capacity;
+		int* data_int;
+		data_int = q ->data;
+		int item;
+		// find the item in data array.
+		item = data_int[head % capacity];
+		// clear the array position.
+		data_int[head % capacity] = -1;
+		head = (head + 1) % capacity;
+		q -> front = head;
+		q ->size --;
+		return item;
+	}
 }
 
 
 // Queue Size
 unsigned int queue_size(queue_t* q){
-	return 0;
+	unsigned int s;
+	s = q ->size;
+	return s;
 }
 
 
 // Free queue
 void free_queue(queue_t* q){
-
+	if (q) {
+		int* data;
+		data = q ->data;
+		free(data);
+		free(q);
+	}
 }
