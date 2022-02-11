cfdbf6ac559d8b8afe0f8bda016efaa1f32361c3
ZetaL0519 <87305674+ZetaL0519@users.noreply.github.com>
2022-02-11 16:36:27 -0500

    Update barebones.s

--- Lab4_Assembly/part4/barebones.s
+++ Lab4_Assembly/part4/barebones.s
@@ -12,29 +12,29 @@ _barebones:
 main:
 					# (1) What are we setting up here?
 					# Ans:
-	pushq %rbp			#
-	movq  %rsp, %rbp		#
+	pushq %rbp			# Push a quadword onto the stack.
+	movq  %rsp, %rbp		# Move a quadword of data froom source to destination.
 
 					# (2) What is going on here
 					# Ans:
-	movq $1, %rax			# 
-	movq $1, %rdi			#
-	leaq .hello.str,%rsi		#
+	movq $1, %rax			# Dereference, and set value to immediate value 1 in register rax.
+	movq $1, %rdi			# Set value 1 to the first argument
+	leaq .hello.str,%rsi		# moves "hello" string into the target register, %rsi(second argument)
 
 
 					# (3) What is syscall? We did not talk about this
 					# in class.
-					# Ans:
+					# Ans: It transfers control to the operating system which then performs the requested service.
 	syscall				# Which syscall is being run?
-					# Ans:
+					# Ans: .text
 
 					# (4) What would another option be instead of 
 					# using a syscall to achieve this?
-					# Ans:
+					# Ans: printf
 
 	movq	$60, %rax		# (5) We are again setting up another syscall
 	movq	$0, %rdi		# What command is it?
-					# Ans:	
+					# Ans:	return 
 	syscall
 
 	popq %rbp			# (Note we do not really need
@@ -43,4 +43,4 @@ main:
 .hello.str:
 	.string "Hello World!\n"
 	.size	.hello.str,13		# (6) Why is there a 13 here?
-					# Ans:	
+					# Ans:	It indicates length of the string
