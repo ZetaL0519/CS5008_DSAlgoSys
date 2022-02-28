3269daabfbc97db6bdb35d306379392e04f05962
Siyue Li <siyue19@login-students.ccs.neu.edu>
2022-02-27 23:06:03 -0500

    Lab Tested

--- Lab6_Processes/Part1/example3.c
+++ Lab6_Processes/Part1/example3.c
@@ -1,1 +1,32 @@
-// Type in and try example3.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    alarm(180); // Terminate the program after 180 seconds.
+
+    // Variable to store the process id
+    pid_t pid;
+    // Variable 'x' allocated on the stack
+    int x = 1;
+
+    // pid returns the child process id.
+    // 0 if it is the child
+    // >0 is the process id of the child which he parent receices
+    pid = fork();
+
+    // Observe this time that the child runs in an infinite loop.
+    if(pid==0){
+        printf("Running child forever now pid=%d\n", getpid());
+	while(1){
+	    printf("Child pid = %d\n Parent pid = %d\n", getpid(),getppid());
+	}
+    }else{
+	// Run the parent, and then shortly terminate it.
+	printf("Terminating parent id=%d\n",getpid());
+	exit(0);
+    }
+    
+    return 0;
+}
--- Lab6_Processes/Part1/myprogram.c
+++ Lab6_Processes/Part1/myprogram.c
@@ -1,1 +1,36 @@
-// Take a look at some of the previous examples to create your own program!
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+    char* argv1[24];
+    char* argv2[24];
+    char* argv3[24];
+
+    argv1[0] = "/bin/ls";
+    argv1[1] = NULL;
+
+    argv2[0] = "/bin/echo";
+    argv2[1] = "'testing'";
+    argv2[2] = NULL;
+
+    argv3[0] = "/bin/nl";
+    argv3[1] = "example5.c";
+    argv3[2] = NULL;
+
+    char** commands[] = {argv1, argv2, argv3};
+    int i;
+    for (i = 0; i < 3; i ++){
+	if (fork()==0){
+	    printf("Running program%d\n",i);
+	    exit(1);
+	}else{
+	    wait(NULL);
+	    printf("Child Pass\n");
+	}
+    }
+    return 0;
+} 
+    
